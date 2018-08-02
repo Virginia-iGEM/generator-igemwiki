be5006b6029cc1c79afb2812bf4aa96e3dbcf8d3
Mantissa-23 <dylanculfogienis@gmail.com>
2018-08-02 10:22:41 -0400

    Copied the generator for the original generator-igemwiki

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,18 +1,149 @@
 var Generator = require('yeoman-generator');
+var fs = require('fs')
+var colors = require('colors')
+var requestSync = require('sync-request')
+var beautify = require('js-beautify').js_beautify
 
 module.exports = class extends Generator {
     constructor(args, opts) {
         super(args, opts);
 
-        this.option('babel'); // Adds a babel option
-    }
+        this.option('skip-install')
+        this.option('skip-repo')
+    };
+    initializing() {
+        // this.log('destinationRoot', this.destinationRoot())
+        // this.log('sourceRoot', this.sourceRoot())
+    };
+    prompting() {
+    return new Promise((resolve, reject) => {
+        var questions = [{
+            type: 'input',
+            name: 'year',
+            message: 'What year is it?',
+            default: (new Date()).getFullYear()
+        }, {
+            type: 'input',
+            name: 'teamName',
+            message: 'What is your team name ' + 'exactly'.red + ' as it appears on the wiki? (Team:' + 'teamName'.magenta + ')',
+            validate: function (input) {
+                if (input === '') {
+                    return false
+                } else {
+                    return true
+                }
+            }
+        }, {
+            type: 'input',
+            name: 'author',
+            message: 'Author? (optional) `Name <email>`'
+        }]
 
-    method1() {
-        this.log('method 1 just ran');
-    }
+        if (!this.options['skip-repo']) {
+            questions.push({
+                type: 'input',
+                name: 'repo',
+                message: 'What is the GitHub repository for this project? (Provided as ' + 'username/repo'.magenta + ')',
+                validate: function (input) {
+                    var good = false
+                    for (var i = 0; i < input.length; i++) {
+                        if (input[i] === '/' && good === false) {
+                            good = true
+                        } else if (input[i] === '/' && good === true) {
+                            good = false
+                            return
+                        }
+                    }
 
-    method2() {
-        this.log('method 2 just ran');
-    }
+                    if (!good)
+                        return 'Follow ' + 'username'.magenta + '/' + 'repo'.magenta + ' format.'
+
+                    base_url = 'https://github.com/'
+
+                    var httpResponse = requestSync('GET', base_url + input)
+
+                    if (httpResponse.statusCode === 200) {
+                        return true
+                    } else if (httpResponse.statusCode === 404) {
+                        return 'Nice try, the page ' + base_url.blue + input.blue + ' returned a ' + '404'.red
+                    } else {
+                        return false
+                    }
+                }
+            })
+        }
+
+        this.prompt(questions, function (answers) {
+            this.config.set({
+                year: answers.year,
+                teamName: answers.teamName,
+                repo: answers.repo ? 'https://github.com/' + answers.repo : '',
+                author: answers.author
+            })
+            resolve()
+        }.bind(this))
+
+    })};
+    configuring() {
+        // this.log('configuring')
+    };
+    default() {
+        // this.log('default')
+    };
+    writing() {
+        this.fs.copyTpl(
+            this.templatePath('bower.json'),
+            this.destinationPath('bower.json'),
+            this.config.getAll()
+        )
+        this.fs.copyTpl(
+            this.templatePath('gulpfile.coffee'),
+            this.destinationPath('gulpfile.coffee'),
+            this.config.getAll()
+        )
+        this.fs.copyTpl(
+            this.templatePath('helpers.coffee'),
+            this.destinationPath('helpers.coffee'),
+            this.config.getAll()
+        )
+        this.fs.copyTpl(
+            this.templatePath('package.json'),
+            this.destinationPath('package.json'),
+            this.config.getAll()
+        )
+        this.fs.copyTpl(
+            this.templatePath('README.md'),
+            this.destinationPath('README.md'),
+            this.config.getAll()
+        )
+        // this.fs.copyTpl(
+        //   this.templatePath('.gitignore'),
+        //   this.destinationPath('.gitignore')
+        // )
+
+        this.directory('src', './src')
+        this.fs.copyTpl(
+            this.templatePath('_template.json'),
+            this.destinationPath('./src/template.json'),
+            this.config.getAll()
+        )
+        this.directory('images', './images')
+    };
+    install() {
+        pkg = JSON.parse(fs.readFileSync('package.json'))
+        pkg.author = this.config.get('author')
+        fs.writeFileSync('package.json', beautify(JSON.stringify(pkg)))
+
+        if (this.options['skip-install']) {
+            this.log('Skipping ' + 'npm install'.magenta + ' and ' + 'bower install'.magenta + '. Run these yourself.')
+        } else {
+            this.installDependencies()
+        }
+    };
+    conflicts() {
+        //this.log('conflicts')
+    };
+    end() {
+        this.log('Good Bye!')
+    };
 };
-
